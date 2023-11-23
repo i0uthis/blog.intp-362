21baca25f46d158dcaf4cc685d3535b47e4bf983
i0uthis <John.Narte@edu.sait.ca>
2023-11-23 14:55:39 -0700

    Update blog2.py

--- docs/blog2/blog2.py
+++ docs/blog2/blog2.py
@@ -1,73 +1,74 @@
 from seal import *
 import random
-
-def print_vector(vector):
-    print('[ ', end='')
-    for i in range(0, len(vector)):
-        print(vector[i], end=', ')
-    print('... ]')
-
-def custom_homomorphic_operations():
-    #setup SEAL parameters
-    parms = EncryptionParameters(scheme_type.bgv)
-    poly_modulus_degree = 8192
-    parms.set_poly_modulus_degree(poly_modulus_degree)
-    parms.set_coeff_modulus(CoeffModulus.BFVDefault(poly_modulus_degree))
-    parms.set_plain_modulus(PlainModulus.Batching(poly_modulus_degree, 20))
-    context = SEALContext(parms)
     
-    #generate keys
-    keygen = KeyGenerator(context)
-    secret_key = keygen.secret_key()
-    public_key = keygen.create_public_key()
-    relin_keys = keygen.create_relin_keys()
-
-    #initiallize encrytor and decryptor
-    encryptor = Encryptor(context, public_key)
-    evaluator = Evaluator(context)
-    decryptor = Decryptor(context, secret_key)
-
-    #initiallize batch encoding for encryption
-    batch_encoder = BatchEncoder(context)
-    slot_count = batch_encoder.slot_count()
-    row_size = slot_count / 2
-    print(f'Plaintext matrix row size: {row_size}')
-
-    #create plaintext string data to be encrypted
-    message = "Hello, This is a secret message."
+def customHE():
+        #setup SEAL parameters
+        parms = EncryptionParameters(scheme_type.bgv)
+        polyModDeg = 8192
+        parms.set_poly_modulus_degree(polyModDeg)
+        parms.set_coeff_modulus(CoeffModulus.BFVDefault(polyModDeg))
+        parms.set_plain_modulus(PlainModulus.Batching(polyModDeg, 20))
+        context = SEALContext(parms)
     
-    #convert message into numbers
-    matrix1 = convert_message_to_numbers(message, slot_count)
-    
-    #print original message
-    print(f"Original Message: {message}")
-    
-    #encode and encrypt plaintexts
-    plain1 = batch_encoder.encode(matrix1)
-    encrypted1 = encryptor.encrypt(plain1)
-    
-    print(f"Ecryption: {encrypted1}")
+        #generate keys
+        keygen = KeyGenerator(context)
+        secretKey = keygen.secret_key()
+        publicKey = keygen.create_public_key()
+        relinKeys = keygen.create_relin_keys()
 
-    #decrypt the encrypted data
-    decrypted_result = decryptor.decrypt(encrypted1)
-    pod_result = batch_encoder.decode(decrypted_result)
+        #initiallize encrytor and decryptor
+        encryptor = Encryptor(context, publicKey)
+        evaluator = Evaluator(context)
+        decryptor = Decryptor(context, secretKey)
 
-    #convert the decrypted values back to a string message
-    decrypted_message = convert_numbers_to_message(pod_result)
+        #initiallize batch encoding for encryption
+        batchEncode = BatchEncoder(context)
+        slotCount = batchEncode.slot_count()
+        rowSize = slotCount / 2
+        print(f'Plaintext matrix row size: {rowSize}\n')
 
-    #print the decrypted message
-    print(f"Decrypted Message: {decrypted_message}")
+        #create plaintext string data to be encrypted
+        msg = ["Hello", "this is a secret message", "you should not have the secret key", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempor, leo a condimentum cursus, odio augue malesuada est, dapibus interdum dui mi et lectus. Vivamus pharetra erat orci. Nunc congue dolor non dolor ultrices tempor. Sed a velit lacinia leo porta dictum nec malesuada justo. Morbi sollicitudin quam sit amet enim lobortis, nec pretium ligula gravida. Ut odio ante, mattis et ante quis, laoreet ornare augue. Morbi ut lacus lacus. Etiam nec est luctus, gravida elit a, fermentum nibh. Vestibulum aliquam nisi non eros lobortis tristique. Maecenas iaculis ex sed eros feugiat sollicitudin. Ut convallis dolor quis leo tempor egestas. Quisque hendrerit odio vitae erat egestas egestas. Sed in commodo risus, nec elementum neque."]
+    
+        concMsg = ' '.join(msg)
+        
+        #convert message into numbers
+        matrix = convMsgtoNum(concMsg, slotCount)
+    
+        #print original message
+        print(f"Original Messages: {msg}\n")
+    
+        #encode messages
+        plain1 = batchEncode.encode(matrix)
+    
+        #encrypt messages
+        encrypted1 = encryptor.encrypt(plain1)
+
+        #decrypt the encrypted data
+        decrypted_result = decryptor.decrypt(encrypted1)
+        podResult = batchEncode.decode(decrypted_result)
+        
+        #print the decrypted message
+        print(f"Decrypted Message: {convNumtoMsg(podResult)}\n")
+        
+        #show characteristics of the ciphertext
+        noise = decryptor.invariant_noise_budget(encrypted1)
+        print(f"Noise budget of ciphertext: {noise} bits\n")
+        size = encrypted1.size()
+        print(f"Size of ciphertext: {2} polynomials\n")
+        params = encrypted1.parms_id()
+        print(f"Encryption parameters of ciphertext: {params}\n")
 
 #function to convert string message to numerical numbers using the ord() function
-def convert_message_to_numbers(message, slot_count):
-    # Convert the message to a numerical representation (plaintext)
-    return [ord(char) for char in message]
+def convMsgtoNum(message, slotCount):
+        # Convert the message to a numerical representation (plaintext)
+        return [ord(char) for char in message]
 
 
-def convert_numbers_to_message(numbers):
-    # Convert numerical representation back to string message
-    return ''.join(chr(number) for number in numbers)
+def convNumtoMsg(numbers):
+        # Convert numerical representation back to string message
+        return ''.join(chr(number) for number in numbers)
 
 
 if __name__ == "__main__":
-    custom_homomorphic_operations()
+        customHE()
